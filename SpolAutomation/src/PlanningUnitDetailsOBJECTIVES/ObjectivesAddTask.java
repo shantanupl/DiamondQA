b3d878b2aedc5fe9bbd996f6f2ad36b55c2a6e62
Shantanu <spaul.automation@gmail.com>
2021-01-11 19:39:24 +0600

    updated one file.

--- SpolAutomation/src/PlanningUnitDetailsOBJECTIVES/ObjectivesAddTask.java
+++ SpolAutomation/src/PlanningUnitDetailsOBJECTIVES/ObjectivesAddTask.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
